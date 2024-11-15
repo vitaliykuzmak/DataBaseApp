338d1966a0e50a27726e43d2e71358de470f9668
vitaliykuzmak <vitalikkuzmak98@gmail.com>
2024-11-15 02:09:12 +0100

    Delete wrong directory

--- src/main/java/DatabaseApp/src/main/java/MaxProjectCountClient.java
+++
@@ -1,9 +0,0 @@
-public class MaxProjectCountClient {
-    private String name;
-    private int projectCount;
-
-    public MaxProjectCountClient(String name, int projectCount) {
-        this.name = name;
-        this.projectCount = projectCount;
-    }
-}
